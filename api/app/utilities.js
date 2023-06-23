cdfe8d6db3ec764bd24da8be7a92192f91826cb8
Aare Puussaar <aare.puussaar@gmail.com>
2023-06-23 12:14:52 +0100

    fix: email redirect links

--- api/app/utilities.js
+++ api/app/utilities.js
@@ -3,14 +3,14 @@ const DEV_URL = 'http://localhost:8888'
 const PROD_URL = `https://*.${process.env.DEPLOYMENT}`
 
 module.exports = {
-  redirectUri: function (instance) {    
+  redirectUri: function (instance) {
     switch (instance) {
       case 'default':
         return DEV_URL
       case 'master':
         return PROD_URL.replace('*.', '')
-      case 'ideaboard':
-        return PROD_URL.replace('*.', '')
+      // case 'ideaboard':
+      //   return PROD_URL.replace('*.', '')
       default:
         return PROD_URL.replace('*', instance)
     }
